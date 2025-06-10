e12f84d256d06bb973bee554a4295092cff0e6b0
King <jafaar.abdulkareem@gmail.com>
2025-06-10 21:45:34 +0530

    update report image and add setting

--- lib/core/constant/app_images.dart
+++ lib/core/constant/app_images.dart
@@ -1,69 +1,45 @@
 // ignore_for_file: prefer_single_quotes
 class AppImages {
   AppImages._();
-
-  /// Assets for imagesCamera
-  /// assets/images/camera.svg
-  static const String imagesCamera = "assets/images/camera.svg";
+  
+  /// Assets for imagesCategory
+  /// assets/images/category.png
+  static const String imagesCategory = "assets/images/category.png";
 
   /// Assets for imagesDelivery
   /// assets/images/delivery.png
   static const String imagesDelivery = "assets/images/delivery.png";
 
-  /// Assets for imagesDiscount
-  /// assets/images/discount.png
-  static const String imagesDiscount = "assets/images/discount.png";
+  /// Assets for imagesMessage
+  /// assets/images/message.png
+  static const String imagesMessage = "assets/images/message.png";
 
-  /// Assets for imagesDragon
-  /// assets/images/dragon.jpg
-  static const String imagesDragon = "assets/images/dragon.jpg";
+  /// Assets for imagesNotification
+  /// assets/images/notification.png
+  static const String imagesNotification = "assets/images/notification.png";
 
-  /// Assets for imagesDress
-  /// assets/images/dress.svg
-  static const String imagesDress = "assets/images/dress.svg";
+  /// Assets for imagesOrder
+  /// assets/images/order.png
+  static const String imagesOrder = "assets/images/order.png";
 
-  /// Assets for imagesLaptop
-  /// assets/images/laptop.svg
-  static const String imagesLaptop = "assets/images/laptop.svg";
+  /// Assets for imagesPayment
+  /// assets/images/payment.png
+  static const String imagesPayment = "assets/images/payment.png";
 
-  /// Assets for imagesLogo
-  /// assets/images/logo.png
-  static const String imagesLogo = "assets/images/logo.png";
+  /// Assets for imagesProduct
+  /// assets/images/product.png
+  static const String imagesProduct = "assets/images/product.png";
 
-  /// Assets for imagesMobile
-  /// assets/images/mobile.svg
-  static const String imagesMobile = "assets/images/mobile.svg";
+  /// Assets for imagesReport
+  /// assets/images/report.png
+  static const String imagesReport = "assets/images/report.png";
 
-  /// Assets for imagesOnboardingfour
-  /// assets/images/onboardingfour.PNG
-  static const String imagesOnboardingfour = "assets/images/onboardingfour.PNG";
+  /// Assets for imagesSetting
+  /// assets/images/setting.png
+  static const String imagesSetting = "assets/images/setting.png";
 
-  /// Assets for imagesOnboardingone
-  /// assets/images/onboardingone.PNG
-  static const String imagesOnboardingone = "assets/images/onboardingone.PNG";
-
-  /// Assets for imagesOnboardingthree
-  /// assets/images/onboardingthree.PNG
-  static const String imagesOnboardingthree =
-      "assets/images/onboardingthree.PNG";
-
-  /// Assets for imagesOnboardingtwo
-  /// assets/images/onboardingtwo.PNG
-  static const String imagesOnboardingtwo = "assets/images/onboardingtwo.PNG";
-
-  /// Assets for imagesPlus
-  /// assets/images/plus.png
-  static const String imagesPlus = "assets/images/plus.png";
-
-  /// Assets for imagesReceive
-  /// assets/images/receive.png
-  static const String imagesReceive = "assets/images/receive.png";
-
-  /// Assets for imagesShirt
-  /// assets/images/shirt.svg
-  static const String imagesShirt = "assets/images/shirt.svg";
-
-  /// Assets for imagesShoes
-  /// assets/images/shoes.svg
-  static const String imagesShoes = "assets/images/shoes.svg";
+  /// Assets for imagesUsers
+  /// assets/images/users.jpg
+  static const String imagesUsers = "assets/images/users.jpg";
 }
+
